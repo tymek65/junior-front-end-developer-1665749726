ce1c84f12df9719766a649f87e42307b36b6f84e
tymek65 <tymek1313@gmail.com>
2022-10-15 23:05:19 +0200

    typo, intl usage for polish translation of timeAgo functon

--- src/components/Main/MessageList.js
+++ src/components/Main/MessageList.js
@@ -1,17 +1,15 @@
 import styles from './MessageList.module.sass';
 import { BusinessContext } from '../../context/BusinessContext';
 import { useContext } from 'react';
-import { coundTimeBetweenDates } from '../../utils/dateHelper';
+import { countTimeBetweenDates } from '../../utils/dateHelper';
 
 const MessageList = () => {
   const tasks = useContext(BusinessContext);
   return (
     <div className={styles.messageListWrap}>
       {tasks[0].context.map((messages, index) => {
-        console.log(messages.created_at);
-        console.log(new Date());
-        coundTimeBetweenDates(messages.created_at, new Date());
-        return <div key={index}>{messages.created_at.toString()}</div>;
+        const time = countTimeBetweenDates(messages.created_at, new Date());
+        return <div key={index}>{time}</div>;
       })}
     </div>
   );
--- src/utils/dateHelper.js
+++ src/utils/dateHelper.js
@@ -1,12 +1,17 @@
-export const coundTimeBetweenDates = (firstDate, secondDate) => {
-  const timeDiff = (secondDate.getTime() - firstDate.getTime()) / 100;
+export const countTimeBetweenDates = (firstDate, secondDate) => {
+  const timeDiff = (secondDate.getTime() - firstDate.getTime()) / 1000;
   const rtf = new Intl.RelativeTimeFormat('pl');
-  const minutes = parseInt(timeDiff / 60);
-  const hours = parseInt(timeDiff / (60 * 60));
-  const days = parseInt(timeDiff / (60 * 60 * 24));
-  const weeks = parseInt(timeDiff / (60 * 60 * 24 * 7));
-  const months = parseInt(timeDiff / (60 * 60 * 24 * 30));
-  const years = parseInt(timeDiff / (60 * 60 * 24 * 365));
-  console.log(years || months || weeks || days || hours || minutes || timeDiff);
-  //   console.log(rtf.format(-12, 'day'));
+  const timePassed = [
+    parseInt(timeDiff / (60 * 60 * 24 * 365)),
+    parseInt(timeDiff / (60 * 60 * 24 * 30)),
+    parseInt(timeDiff / (60 * 60 * 24 * 7)),
+    parseInt(timeDiff / (60 * 60 * 24)),
+    parseInt(timeDiff / (60 * 60)),
+    parseInt(timeDiff / 60),
+    parseInt(timeDiff),
+  ];
+  const time = ['year', 'month', 'weeks', 'days', 'hours', 'minutes', 'seconds'];
+  for (let i = 0; i < timePassed.length; i++) {
+    if (timePassed[i] > 0) return rtf.format(-timePassed[i], time[i]);
+  }
 };
