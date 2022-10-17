07a0e5965dabf08c84b133016e6f1f0d063464b8
tymek65 <tymek1313@gmail.com>
2022-10-17 02:43:21 +0200

    explanation about specific code solutions

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -1,3 +1,9 @@
+//in working environment with api connected to the app importing/exporting data as in my project wouldn't be possible
+//but I think it was pointless to mimic working with api, one of ways of doing same thing would be to just
+//get specific information I need in given component with graphql query and then do another graphql query with
+//specific task id to get business context
+//while working with rest api we could store the data in context (like i was doing couple commits ago) or drill them down to components that needs them
+
 export const tasks = [
   {
     name: 'Application Setup',
@@ -24,7 +30,7 @@ export const tasks = [
     context: [
       {
         author: 'Kristen Aniston',
-        title: 'Application has been accepted',
+        title: 'Application has been accepted 🎉 🙌',
         created_at: new Date('2022-10-11T10:20:00'),
         content:
           'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat nobis temporibus laboriosam quidem eum nostrum a non maiores quasi quis, molestias hic incidunt at consectetur blanditiis omnis laborum voluptatem. Illum!',
@@ -51,14 +57,14 @@ export const tasks = [
       {
         author: 'Olga Nelson',
         title: 'Application has been accepted',
-        created_at: new Date('2022-10-14T14:20:00'),
+        created_at: new Date('2022-10-03T12:22:00'),
         content:
           'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat nobis temporibus laboriosam quidem eum nostrum a non maiores quasi quis, molestias hic incidunt at consectetur blanditiis omnis laborum voluptatem. Illum!',
       },
       {
         author: 'Kristen Aniston',
         title: 'New sprint, tasks, and intro information',
-        created_at: new Date('2022-10-13T12:13:00'),
+        created_at: new Date('2022-10-05T16:37:00'),
         content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam saepe animi ipsa modi totam, alias vitae vero, quisquam ad quaerat eligendi ut itaque.',
       },
     ],
@@ -70,15 +76,16 @@ export const tasks = [
       {
         author: 'Olga Nelson',
         title: 'Application has been accepted',
-        created_at: new Date('2022-10-14T14:20:00'),
+        created_at: new Date('2022-09-09T20:20:00'),
         content:
           'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat nobis temporibus laboriosam quidem eum nostrum a non maiores quasi quis, molestias hic incidunt at consectetur blanditiis omnis laborum voluptatem. Illum!',
       },
       {
         author: 'Kristen Aniston',
         title: 'New sprint, tasks, and intro information',
-        created_at: new Date('2022-10-13T12:13:00'),
-        content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam saepe animi ipsa modi totam, alias vitae vero, quisquam ad quaerat eligendi ut itaque.',
+        created_at: new Date('2022-09-10T19:13:00'),
+        content:
+          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, illo aliquam eligendi magni suscipit labore! Hic vero, harum assumenda laudantium labore ea quidem sed ullam! Inventore nemo temporibus cum totam voluptates assumenda saepe alias sed, cumque iusto eum tenetur. Optio, cupiditate expedita, nisi incidunt rem dignissimos harum facere culpa atque laboriosam ut. Asperiores molestiae enim quos cumque velit quasi magnam nisi unde repellat at nihil totam, quam facere obcaecati quas perspiciatis minima doloremque numquam atque eum ducimus quisquam reprehenderit quis? Totam soluta repudiandae delectus quasi vero laboriosam exercitationem, recusandae a, impedit natus beatae voluptates necessitatibus, fugit aperiam dolores voluptas cumque?',
       },
     ],
   },
